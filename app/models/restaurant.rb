0887dd59ddc6a98f0fd9df1fc58619d7dcd4a460
Joe Smith <joeycrushed@gmail.com>
2021-05-26 12:04:16 +0100

    Creating Restaurant & Reviews Models with validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,10 @@
 class Restaurant < ApplicationRecord
+has_many :reviews, dependent: :destroy
+
+  FOODS = %w(chinese italian japanese french belgian)
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :name, presence: true
+  validates :category, inclusion: { in: FOODS }
 end
